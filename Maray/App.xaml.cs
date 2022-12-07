936574fbb8f16b3d7406ffde475f2c58236b2ac7
NewUranus <mr.uranus@outlook.com>
2022-12-07 20:23:06 +0800

    create project

---
+++ Maray/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maray;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maray/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maray;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
