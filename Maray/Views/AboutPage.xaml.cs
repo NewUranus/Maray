2bfc2c79c53fcc61e90caab831a779e5d63c70ea
潘文博 <964012840@qq.com>
2022-12-08 21:35:36 +0800

    新建关于对话框

---
+++ Maray/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Maray.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://github.com/NewUranus/Maray");
+    }
+}
