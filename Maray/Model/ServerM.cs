02b3ce7d0fbc26675080de2a079c91271273c896
NewUranus <mr.uranus@outlook.com>
2022-12-09 20:44:19 +0800

    add server model

---
+++ Maray/Model/ServerM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Maray.Model
+{
+    public class ServerM
+    {
+        public string v { get; set; }
+        public string ps { get; set; }
+        public string add { get; set; }
+        public string port { get; set; }
+        public string type { get; set; }
+        public string id { get; set; }
+        public string aid { get; set; }
+        public string net { get; set; }
+        public string path { get; set; }
+        public string host { get; set; }
+        public string tls { get; set; }
+    }
+}
+
--- Maray/Views/Server.xaml.cs
+++ Maray/Views/Server.xaml.cs
@@ -1,4 +1,6 @@
 ﻿namespace Maray.Views;
+using System;
+using System.Text;
 
 public partial class Server : ContentPage
 {
@@ -6,4 +8,13 @@ public partial class Server : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    void OnEntryCompleted(object sender, EventArgs e)
+    {
+        ((Entry)sender).Text = Encoding.Default.GetString(Convert.FromBase64String(((Entry)sender).Text));
+    }
+
+    void OnEntryTextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+    }
 }
